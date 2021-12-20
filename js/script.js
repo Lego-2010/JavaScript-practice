62026faeb3b0e0995677b62f6d18acc56062722c
Anastasya <Nassska-2015@mail.ru>
2021-12-20 18:28:18 +0300

    first practically-focused assignment

--- js/script.js
+++ js/script.js
@@ -1,9 +1,18 @@
-"use strict";
+const number0fFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+const personalMovieDB = {
+    count: number0fFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
 
-const answers = [];
-answers[0] = prompt('Как ваше имя?', '');
-answers[1] = prompt('Как ваше фамилия?', '');
-answers[2] = prompt('Сколько вам лет?', '');
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
 
-console.log(typeof(answers));
-console.log(typeof(null));
+      personalMovieDB.movies[a] = b;
+      personalMovieDB.movies[c] = d;
+
+      console.log(personalMovieDB);
